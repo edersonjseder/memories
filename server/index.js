f499baa1d0580201dabef10667778b8b959fd46c
Ederson de Jesus <edersonjseder@gmail.com>
2022-10-25 21:47:52 +0200

    Prepare for deployment

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ import bodyParser from "body-parser";
 import cors from "cors";
 import dotenv from "dotenv";
 import connectDB from "./config/database.js";
+import path from "path";
 
 import postRoutes from "./routes/postsRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
@@ -16,8 +17,17 @@ app.use(cors());
 app.use("/posts", postRoutes);
 app.use("/user", userRoutes);
 
-app.get("/", (req, res) => {
-  res.send("APP IS RUNNING...");
-});
+const __dirname = path.resolve();
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/client/build")));
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("APP IS RUNNING...");
+  });
+}
 
 connectDB(app);
