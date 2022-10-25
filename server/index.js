ae572fe17ed7b4ae7be64215c18dea51a4e4e3ed
Ederson de Jesus <edersonjseder@gmail.com>
2022-10-25 23:04:28 +0200

    Prepare for deployment 8

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ import postRoutes from "./routes/postsRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
 
 const app = express();
-dotenv.config();
+// dotenv.config();
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
