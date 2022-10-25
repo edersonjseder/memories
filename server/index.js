4179c8b58d7c5ecbe53ff07b4ca5a4a2c8be194e
Ederson de Jesus <edersonjseder@gmail.com>
2022-10-25 23:13:30 +0200

    Prepare for deployment 9

--- server/index.js
+++ server/index.js
@@ -1,15 +1,15 @@
 import express from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
-// import dotenv from "dotenv";
+import dotenv from "dotenv";
 import connectDB from "./config/database.js";
 import path from "path";
 
 import postRoutes from "./routes/postsRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
 
+dotenv.config();
 const app = express();
-// dotenv.config();
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
